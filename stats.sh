522a2b42c0faac3a373a6f6d6b662b65817fd08b
Noran04 <noaljodi04@gmail.com>
2025-05-14 15:02:41 +0000

    Noran added mode/max/min

--- stats.sh
+++ stats.sh
@@ -22,4 +22,33 @@ read -p "Enter choice [1-6]: " choice
 read -p "Which column numbers? (e.g., 1 2 3): " columns
 
 
+case $choice in 
+
+
+   1)
+
+
+   2)
+    for col in $columns; do
+      awk -v c="$col" '{freq[$c]++} END{for(val in freq){if(freq[val]>max){max=freq[val];mode=val}} print "Mode of column", c, "=", mode, "(", max, "times)"}' cleaned_data.txt
+    done
+    ;;
+
+
+  3)
+    for col in $columns; do
+      awk -v c="$col" 'NR==1 {min=max=$c} {if($c<min)min=$c; if($c>max)max=$c} END{print "Column", c, "-> Min =", min, ", Max =", max}' cleaned_data.txt
+    done
+    ;;
+    
+  4)
+    
+    
+  5)
+    
+
+  6)
+  
+  esac
+
 
